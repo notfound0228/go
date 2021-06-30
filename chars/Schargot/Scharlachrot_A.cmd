e6937467cf9bf81ceff88aa0b381d41d9443aedf
notfound0228 <70077004+notfound0228@users.noreply.github.com>
2021-07-01 01:42:43 +0900

    Add files via upload

---
+++ chars/Schargot/Scharlachrot_A.cmd
@@ -0,0 +1,912 @@
+;-| 超必殺技 |---------------------------------------------------
+[Command]
+name = "gyakuyoga_c"
+command = ~F, D, B, c
+time = 17
+
+[Command]
+name = "tatsumaki_c"
+command = ~D, DB, B, c
+time = 20
+
+[Command]
+name = "hado_c"
+command = ~D, F, c
+time = 20
+
+[Command]
+name = "death"
+command = ~D, F, D, F, s
+time = 25
+
+[Command]
+name = "tatsumaki2_x"
+command = ~D, B, D, B, x
+time = 25
+
+[Command]
+name = "tatsumaki2_x"
+command = ~D, B, D, B, y
+time = 25
+
+[Command]
+name = "tatsumaki2_x"
+command = ~D, B, D, B, z
+time = 25
+
+[Command]
+name = "tatsumaki2_a"
+command = ~D, B, D, B, a
+time = 25
+
+[Command]
+name = "tatsumaki2_a"
+command = ~D, B, D, B, b
+time = 25
+
+[Command]
+name = "tatsumaki2_a"
+command = ~D, B, D, B, c
+time = 25
+
+[Command]
+name = "hado2_x"
+command = ~D, F, D, F, x
+time = 25
+
+[Command]
+name = "hado2_x"
+command = ~D, F, D, F, y
+time = 25
+
+[Command]
+name = "hado2_x"
+command = ~D, F, D, F, z
+time = 25
+
+[Command]
+name = "hado2_a"
+command = ~D, F, D, F, a
+time = 25
+
+[Command]
+name = "hado2_a"
+command = ~D, F, D, F, b
+time = 25
+
+[Command]
+name = "hado2_a"
+command = ~D, F, D, F, c
+time = 25
+
+[Command]
+name = "D3_c"
+command = ~D, D, D, c
+time = 25
+
+[Command]
+name = "D3_ab"
+command = ~D, D, D, a+b
+time = 25
+
+;-| 必殺技 |---------------------------------------------------
+[Command]
+name = "hado_a"
+command = ~D, DF, F, a
+time = 20
+
+[Command]
+name = "hado_a"
+command = ~D, DF, F, b
+time = 20
+
+[Command]
+name = "hado_a"
+command = ~D, DF, F, c
+time = 20
+
+[Command]
+name = "hado_x"
+command = ~D, DF, F, x
+time = 20
+
+[Command]
+name = "hado_y"
+command = ~D, DF, F, y
+time = 20
+
+[Command]
+name = "hado_z"
+command = ~D, DF, F, z
+time = 20
+
+[Command]
+name = "hado_xy"
+command = ~D, DF, F, x+y
+time = 20
+
+[Command]
+name = "tatsumaki_a"
+command = ~D, DB, B, a
+time = 20
+
+[Command]
+name = "tatsumaki_a"
+command = ~D, DB, B, b
+time = 20
+
+[Command]
+name = "tatsumaki_a"
+command = ~D, DB, B, c
+time = 20
+
+[Command]
+name = "tatsumaki_x"
+command = ~D, DB, B, x
+time = 20
+
+[Command]
+name = "tatsumaki_y"
+command = ~D, DB, B, y
+time = 20
+
+[Command]
+name = "tatsumaki_z"
+command = ~D, DB, B, z
+time = 20
+
+[Command]
+name = "gyakuyoga_a"
+command = ~F, D, B, a
+time = 17
+
+[Command]
+name = "gyakuyoga_b"
+command = ~F, D, B, b
+time = 17
+
+[Command]
+name = "gyakuyoga_ab"
+command = ~F, D, B, a+b
+time = 17
+
+[Command]
+name = "gyakuyoga_x"
+command = ~F, D, B, x
+time = 17
+
+[Command]
+name = "gyakuyoga_y"
+command = ~F, D, B, y
+time = 17
+
+[Command]
+name = "yoga_ab"
+command = ~B, D, F, a+b
+time = 17
+
+[Command]
+name = "yoga_xy"
+command = ~B, D, F, x+y
+time = 17
+
+[Command]
+name = "yoga_a"
+command = ~B, D, F, a
+time = 17
+
+[Command]
+name = "yoga_b"
+command = ~B, D, F, b
+time = 17
+
+[Command]
+name = "yoga_x"
+command = ~B, D, F, x
+time = 17
+
+[Command]
+name = "yoga_y"
+command = ~B, D, F, y
+time = 17
+
+;-| ２回押し技 |-----------------------------------------------
+[Command]
+name = "FF"     
+command = F, F
+time = 10
+
+[Command]
+name = "BB"     
+command = B, B
+time = 10
+
+[Command]
+name = "SS"     
+command = s, s, s
+time = 20
+
+;-| ２・３個の同時押し技 |-------------------------------------
+[Command]
+name = "recovery"
+command = x+y
+time = 1
+
+[Command]
+name = "dizzyrecover"
+command = F,B
+time = 20
+
+[Command]
+name = "recovery"
+command = a+b
+time = 1
+
+[Command]
+name = "recovery"
+command = x+a
+time = 1
+
+[Command]
+name = "recovery"
+command = y+b
+time = 1
+
+[Command]
+name = "b+y"
+command = b+y
+time = 1
+
+[Command]
+name = "a+x"
+command = a+x
+time = 1
+
+[Command]
+name = "a+y"
+command = a+y
+time = 1
+
+[Command]
+name = "x+b"
+command = x+b
+time = 1
+
+;-| 方向とボタンで出す技 |-------------------------------------
+[command]
+name = "fwd_a"
+command = /F, a
+time = 1
+
+[command]
+name = "fwd_b"
+command = /F, b
+time = 1
+
+[command]
+name = "fwd_c"
+command = /F, c
+time = 1
+
+[command]
+name = "fwd_x"
+command = /F, x
+time = 1
+
+[command]
+name = "fwd_y"
+command = /F, y
+time = 1
+
+[command]
+name = "fwd_z"
+command = /F, z
+time = 1
+
+[command]
+name = "back_a"
+command = /B, a
+time = 1
+
+[command]
+name = "back_b"
+command = /B, b
+time = 1
+
+[command]
+name = "back_c"
+command = /B, c
+time = 1
+
+[command]
+name = "back_x"
+command = /B, x
+time = 1
+
+[command]
+name = "back_y"
+command = /B, y
+time = 1
+
+[command]
+name = "back_z"
+command = /B, z
+time = 1
+
+[Command]
+name = "down_a"
+command = /$D,a
+time = 1
+
+[Command]
+name = "down_b"
+command = /$D,b
+time = 1
+
+[Command]
+name = "down_c"
+command = /$D,b
+time = 1
+
+[Command]
+name = "down_x"
+command = /$D,x
+time = 1
+
+[Command]
+name = "down_y"
+command = /$D,y
+time = 1
+
+[Command]
+name = "down_z"
+command = /$D,z
+time = 1
+
+[command]
+name = "downfwd_a"
+command = /DF, a
+time = 1
+
+[command]
+name = "downfwd_y"
+command = /DF, y
+time = 1
+
+[Command]
+name = "up_a"
+command = /$U,a
+time = 1
+
+[Command]
+name = "up_b"
+command = /$U,b
+time = 1
+
+[Command]
+name = "6s"
+command = /F,s
+time = 1
+
+[Command]
+name = "4s"
+command = /B,s
+time = 1
+
+;-| ボタン設定（いじらない）|----------------------------------
+[Command]
+name = "a"
+command = a
+time = 1
+
+[Command]
+name = "b"
+command = b
+time = 1
+
+[Command]
+name = "c"
+command = c
+time = 1
+
+[Command]
+name = "x"
+command = x
+time = 1
+
+[Command]
+name = "y"
+command = y
+time = 1
+
+[Command]
+name = "z"
+command = z
+time = 1
+
+[Command]
+name = "start"
+command = s
+time = 1
+
+;-| 押しっぱなし設定（いじらない）-----------------------------
+[Command]
+name = "holdfwd"
+command = /$F
+time = 1
+
+[Command]
+name = "holdback"
+command = /$B
+time = 1
+
+[Command]
+name = "holdup" 
+command = /$U
+time = 1
+
+[Command]
+name = "holddown"
+command = /$D
+time = 1
+
+[Command]
+name = "longjump"
+command = D, $U
+
+[Command]
+name = "hold_a"
+command = /$a
+time = 1
+
+[Command]
+name = "hold_b"
+command = /$b
+time = 1
+
+[Command]
+name = "hold_c"
+command = /$c
+time = 1
+
+[Command]
+name = "hold_x"
+command = /$x
+time = 1
+
+[Command]
+name = "hold_y"
+command = /$y
+time = 1
+
+[Command]
+name = "hold_z"
+command = /$z
+time = 1
+
+[Command]
+name = "hold_y+b"
+command = /$y+b
+time = 1
+
+[command]
+name="fwd"
+command=F
+time=1
+[command]
+name="back"
+command=B
+time=1
+[command]
+name="up"
+command=U
+time=1
+[command]
+name="down"
+command=D
+time=1
+
+;-|Commands|------------------------------------------------------------------------------
+[Statedef -1]
+
+;--------------------------------------------------------------
+
+;--------------------------------------------------------------
+; ダッシュ
+[State -1, Run Fwd]
+type = ChangeState
+value = 100
+triggerall = command = "FF"
+trigger1 = statetype = S
+trigger1 = Stateno != 100
+trigger1 = Stateno != 101
+trigger1 = ctrl
+
+;--------------------------------------------------------------
+; バックステップ
+[State -1, Run Back]
+type = ChangeState
+value = 105
+trigger1 = command = "BB"
+trigger1 = statetype = S
+trigger1 = ctrl
+
+;--------------------------------------------------------------
+;12P&AI用受身
+[State -1]
+type = ChangeState
+value = 5201
+triggerall = var(39) = 1
+triggerall = alive
+triggerall = Command = "recovery"
+triggerall = stateno = 5050
+trigger1 = pos y >= -45
+
+;---------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+;超必殺技;============================================================
+;---------------------------------------------------------------------------------------------------------------------------
+;---------------------------------------------------------------------------------------------------------------------------
+;---------------------------------------------------------------------------------------------------------------------------
+
+
+;-------------------------------------------------------------
+;デスグラインダー
+[State -1]
+type = ChangeState
+value = 1500
+triggerall = var(59)<=0
+triggerall = command = "start"
+triggerall = power >= 2000
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;デスグラインダー
+[State -1]
+type = ChangeState
+value = 1700
+triggerall = var(59)<=0
+triggerall = command = "start"
+triggerall = enemy,life <= 300
+triggerall = enemy,movetype = H
+triggerall = enemy,anim = 1555
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;コラプトシェル　EX
+[State -1]
+type = ChangeState
+value = 1030
+triggerall = var(59)<=0
+triggerall = command = "fwd_y"
+triggerall = power >= 1000
+triggerall = numhelper(1050) = 0
+triggerall = numhelper(1060) = 0
+triggerall = numhelper(1070) = 0
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;パージ　EX
+[State -1]
+type = ChangeState
+value = 1111
+triggerall = var(59)<=0
+triggerall = command = "down_y"
+triggerall = power >= 1000
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;ディスデイン　EX
+[State -1]
+type = ChangeState
+value = 1240
+triggerall = var(59)<=0
+triggerall = command = "back_y"
+triggerall = power >= 1000
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;ロックオン　EX
+[State -1]
+type = null;ChangeState
+value = 1330
+triggerall = var(59)<=0
+triggerall = command = "tatsumaki2_a"
+trigger1 = statetype = S
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;コラプトシェル　EX
+[State -1]
+type = ChangeState
+value = 1030
+triggerall = var(59)<=0
+triggerall = command = "y"
+triggerall = power >= 1000
+triggerall = numhelper(1050) = 0
+triggerall = numhelper(1060) = 0
+triggerall = numhelper(1070) = 0
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+
+
+
+
+;必殺技;============================================================
+;---------------------------------------------------------------------------------------------------------------------------
+;---------------------------------------------------------------------------------------------------------------------------
+
+;-------------------------------------------------------------
+;コラプトシェル　弱
+[State -1]
+type = ChangeState
+value = 1000
+triggerall = var(59)<=0
+triggerall = command = "fwd_x"
+triggerall = numhelper(1050) = 0
+triggerall = numhelper(1060) = 0
+triggerall = numhelper(1070) = 0
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;コラプトシェル　中
+[State -1]
+type = null;ChangeState
+value = 1010
+triggerall = var(59)<=0
+triggerall = command = "fwd_x"
+trigger1 = statetype = S
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;コラプトシェル　強
+[State -1]
+type = null;ChangeState
+value = 1020
+triggerall = var(59)<=0
+triggerall = command = "fwd_x"
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;パージ
+[State -1]
+type = ChangeState
+value = 1100
+triggerall = var(59)<=0
+triggerall = command = "down_x"
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;ディスデイン　弱
+[State -1]
+type = ChangeState
+value = 1200
+triggerall = var(59)<=0
+triggerall = command = "back_x"
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;ディスデイン　中
+[State -1]
+type = null;ChangeState
+value = 1220
+triggerall = var(59)<=0
+triggerall = command = "tatsumaki_y"
+trigger1 = statetype = S
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;ディスデイン　強
+[State -1]
+type = null;ChangeState
+value = 1230
+triggerall = var(59)<=0
+triggerall = command = "tatsumaki_z"
+trigger1 = statetype = S
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;ロックオン
+[State -1]
+type = null;ChangeState
+value = 1300
+triggerall = var(59)<=0
+triggerall = command = "tatsumaki_a"
+trigger1 = statetype = S
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;コラプトシェル　弱
+[State -1]
+type = ChangeState
+value = 1000
+triggerall = var(59)<=0
+triggerall = command = "x"
+triggerall = numhelper(1050) = 0
+triggerall = numhelper(1060) = 0
+triggerall = numhelper(1070) = 0
+trigger1 = statetype != A
+trigger1 = ctrl = 1
+
+
+
+
+;通常技;============================================================
+;---------------------------------------------------------------------------------------------------------------------------
+
+;--------------------------------------------------------------
+; Ｐ＆Ｋ投げ
+[State -1,Throw]
+type = ChangeState
+value = 800
+triggerall = command = "c"
+triggerall = statetype = S
+triggerall = ctrl
+triggerall = stateno != 100
+;trigger1 = command = "holdfwd"
+trigger1 = p2bodydist X < 5
+trigger1 = (p2statetype = S) || (p2statetype = C)
+trigger1 = p2movetype != H
+;trigger2 = command = "holdback"
+trigger2 = p2bodydist X < 5
+trigger2 = (p2statetype = S) || (p2statetype = C)
+trigger2 = p2movetype != H
+trigger3 = stateno = 100 || stateno = 103
+
+;-------------------------------------------------------------
+;立弱P
+[State -1]
+type = ChangeState
+value = 200
+triggerall = var(59)<=0
+triggerall = command = "a"
+triggerall = command != "holddown"
+trigger1 = statetype = S
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;立中P
+[State -1]
+type = ChangeState
+value = 210
+triggerall = var(59)<=0
+triggerall = command = "a"
+triggerall = command != "holddown"
+trigger1 = statetype = S
+Trigger1 = StateNo = 200 && MoveContact
+
+;-------------------------------------------------------------
+;立強P
+[State -1]
+type = ChangeState
+value = 220
+triggerall = var(59)<=0
+triggerall = command = "a"
+triggerall = command != "holddown"
+trigger1 = statetype = S
+Trigger1 = StateNo = 210 && MoveContact
+
+;-------------------------------------------------------------
+;立強P
+[State -1]
+type = ChangeState
+value = 220
+triggerall = var(59)<=0
+triggerall = command = "b"
+triggerall = command != "holddown"
+trigger1 = statetype = S
+trigger1 = ctrl = 1
+Trigger2 = StateNo = 210 && MoveContact
+
+;-------------------------------------------------------------
+;オートアタック
+[State -1]
+type = ChangeState
+value = 230
+triggerall = var(59)<=0
+triggerall = command = "z"
+triggerall = statetype != A
+triggerall = command != "holddown"
+trigger1 = ctrl = 1
+Trigger2 = StateNo = 220 && MoveContact
+
+;-------------------------------------------------------------
+;オートアタック
+[State -1]
+type = ChangeState
+value = 231
+triggerall = var(59)<=0
+triggerall = command = "z"
+triggerall = statetype != A
+triggerall = command = "holddown"
+trigger1 = ctrl = 1
+Trigger2 = StateNo = 420 && MoveContact
+
+;-------------------------------------------------------------
+;屈弱P
+[State -1]
+type = ChangeState
+value = 400
+triggerall = var(59)<=0
+triggerall = command = "a"
+triggerall = command = "holddown"
+trigger1 = statetype = C
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;屈中P
+[State -1]
+type = ChangeState
+value = 410
+triggerall = var(59)<=0
+triggerall = command = "a"
+triggerall = command = "holddown"
+trigger1 = statetype = C
+Trigger1 = StateNo = 400 && MoveContact
+
+;-------------------------------------------------------------
+;屈強P
+[State -1]
+type = ChangeState
+value = 420
+triggerall = var(59)<=0
+triggerall = command = "a"
+triggerall = command = "holddown"
+trigger1 = statetype = C
+Trigger1 = StateNo = 410 && MoveContact
+
+;-------------------------------------------------------------
+;屈強P
+[State -1]
+type = ChangeState
+value = 420
+triggerall = var(59)<=0
+triggerall = command = "b"
+triggerall = command = "holddown"
+trigger1 = statetype = C
+trigger1 = ctrl = 1
+Trigger2 = StateNo = 410 && MoveContact
+
+;-------------------------------------------------------------
+;空弱P
+[State -1]
+type = null;ChangeState
+value = 600
+triggerall = var(59)<=0
+triggerall = command = "a"
+trigger1 = statetype = A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;空中P
+[State -1]
+type = ChangeState
+value = 610
+triggerall = var(59)<=0
+triggerall = command = "a"
+trigger1 = statetype = A
+trigger1 = ctrl = 1
+
+;-------------------------------------------------------------
+;空強P
+[State -1]
+type = ChangeState
+value = 620
+triggerall = var(59)<=0
+triggerall = command = "b"
+trigger1 = statetype = A
+trigger1 = ctrl = 1
